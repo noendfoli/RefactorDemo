1c636a749941a3703f3d76a4ef2f394e46650c11
owen <649073762@qq.com>
2019-08-12 21:56:20 +0800

    在SulfurStrangely类中抽出whenItemQualitySmallThan0()

--- src/main/java/gildedRose/SulfurasStrategy.java
+++ src/main/java/gildedRose/SulfurasStrategy.java
@@ -3,13 +3,15 @@ package gildedRose;
 public class SulfurasStrategy extends ChangeStrategy {
     @Override
     void changeItem(Item item) {
+        whenItemQualitySmallThan0(item);
+        if (item.sellIn < 0) {
+            whenItemQualitySmallThan0(item);
+        }
+    }
+
+    private void whenItemQualitySmallThan0(Item item) {
         if (item.quality > 0) {
             item.quality = item.quality - 1;
-        }
-        if (item.sellIn < 0) {
-            if (item.quality > 0) {
-                item.quality = item.quality - 1;
-            }
         }
     }
 }
