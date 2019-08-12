d05b36598d82a16ccd684b8fecba0da3fff23618
owen <649073762@qq.com>
2019-08-12 22:03:47 +0800

    在初始化Item时，添加策略，使用策略者模式

--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -8,10 +8,20 @@ public class Item {
 
     public int quality;
 
+    public ChangeStrategy changeStrategy;
     public Item(String name, int sellIn, int quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
+        if(name.equals("Aged Brie")){
+            this.changeStrategy = new AgedStrategy();
+        }else if(name.equals("Backstage passes to a TAFKAL80ETC concert")){
+            this.changeStrategy = new BackstageStrategy();
+        }else if(name.equals("Sulfuras, Hand of Ragnaros")){
+            this.changeStrategy = new SulfurasStrategy();
+        }else {
+
+        }
     }
 
 }
