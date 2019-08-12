a13dcbbf89c9838da1ad694bf0bafc8a7c1331a5
owen <649073762@qq.com>
2019-08-12 21:38:15 +0800

    AgedStrategy中的当Qualtiy小于50，抽出方法

--- src/main/java/gildedRose/AgedStrategy.java
+++ src/main/java/gildedRose/AgedStrategy.java
@@ -3,14 +3,16 @@ package gildedRose;
 public class AgedStrategy extends ChangeStrategy {
     @Override
     void changeItem(Item item) {
-        if (item.quality < 50) {
-            item.quality = item.quality + 1;
-        }
+        whenItemQualitySmallThan50(item);
         item.sellIn = item.sellIn - 1;
         if (item.sellIn < 0) {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
-            }
+            whenItemQualitySmallThan50(item);
+        }
+    }
+
+    private void whenItemQualitySmallThan50(Item item) {
+        if (item.quality < 50) {
+            item.quality = item.quality + 1;
         }
     }
 }
